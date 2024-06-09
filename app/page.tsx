eafc0fe737cfb3efa319cb8ce81eb9239296cbf5
Ahmed Mahmood <ahmedmahmood208@gmail.com>
2024-06-09 11:26:45 +0100

    change dir structure

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Home from "@/app/home";
+
+export default function Index() {
+  return (
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Home></Home>
+    // </main>
+  );
+}
