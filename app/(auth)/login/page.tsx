affb45bfd1202e84942d801cb948d597147a3541
Ahmed Mahmood <ahmedmahmood208@gmail.com>
2024-06-09 23:31:14 +0100

    add suspense boundry to Login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,12 +1,14 @@
 import LoginForm from "@/components/login-form";
 import Link from "next/link";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
     <>
       <h1>Login</h1>
-
+      <Suspense fallback={<div>Loading...</div>}>
       <LoginForm />
+    </Suspense>
 
       <ul>
         <li>
