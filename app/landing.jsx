5b490ad89e1820b1c0514213739577f4fe4521ab
DeeBabaTech <dee1dayo4real@gmail.com>
2024-09-05 22:29:23 +0100

    changes

--- app/landing.jsx
+++ app/landing.jsx
@@ -1,11 +1,11 @@
-import Header from "../components/header/header";
-import Hero from "../components/hero/hero";
-import Join from "../components/join/join";
-import Service from "../components/service/service";
-import Freelance from "../components/freelancer/freelance";
-import Connect from "../components/connect/connect";
-import Subscribe from "../components/subscribe/subscribe";
-import Footer from "../components/footer/footer";
+import Header from "../components/landingpage/header/header";
+import Hero from "../components/landingpage/hero/hero";
+import Join from "../components/landingpage/join/join";
+import Service from "../components/landingpage/service/service";
+import Freelance from "../components/landingpage/freelance/freelance";
+import Connect from "../components/landingpage/connect";
+import Subscribe from "../components/landingpage/subscribe";
+import Footer from "../components/landingpage/footer";
 
 const Landing = () => {
   return (
